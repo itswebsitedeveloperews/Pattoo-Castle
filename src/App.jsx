1fbf535caedfd25231c3470ba34afce8fba848b0
chiragews-web <chirag.ews@gmail.com>
2026-09-02 15:34:10 +0530

    css

--- src/App.jsx
+++ src/App.jsx
@@ -527,19 +527,21 @@ function App({ footerEntry = null, headerEntry = null, homePageEntry = null }) {
                 <div className="hero-actions">
                   {hasPrimaryButton && (
                     <a
-                      className="button button--light hero-button"
+                      className="button button--light hero-button hero-button--primary"
                       href={homePage.buttonUrl}
                     >
                       {homePage.buttonText}
+                      <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 18L18 6M18 6H9M18 6V15" stroke="#513624" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                     </a>
                   )}
                   {hasSecondaryButton && (
                     <a
-                      className="button button--light hero-button"
+                      className="button button--light hero-button hero-button--secondary"
                       href={homePage.button2Url}
                       target="_blank"
                     >
                       {homePage.button2Text}
+                      <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 18L18 6M18 6H9M18 6V15" stroke="#FFFFFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                     </a>
                   )}
                 </div>
